466692c4d1b4fdf26c35a788ed09401362208be6
Stefan Rijnhart <stefan@opener.am>
2021-12-28 19:34:30 +0100

    [FIX] Admin name is Mitch Admin in later versions

--- tests_with_odoo/test_session.py
+++ tests_with_odoo/test_session.py
@@ -1,11 +1,6 @@
 from unittest import TestCase
 from oca.recipe.odoo.runtime.session import Session
-try:
-    from odoo.tests.common import get_db_name
-    from odoo.release import version_info
-except ImportError:
-    from openerp.tests.common import get_db_name
-    from openerp.release import version_info
+from odoo.tests.common import get_db_name
 
 TEST_MODULE = 'web_environment_ribbon'
 
@@ -33,26 +28,13 @@ class SessionTestCase(TestCase):
         self.session.open(db=get_db_name())
 
     def test_env_after_install_module(self):
-        self.assertAdminPresentWithV8API()
+        self.assertAdminPresent()
         self.session.install_modules([TEST_MODULE])
-        self.assertAdminPresentWithV8API()
+        self.assertAdminPresent()
 
-    def assertAdminPresentWithV7API(self):
-        user_mdl = self.session.registry('res.users')
-        admin_id = user_mdl.search(
-            self.session.cr, self.session.uid, [('login', '=', 'admin')]
-        )[0]
-
-        self.assertEqual(
-            u"Administrator",
-            user_mdl.read(
-                self.session.cr, self.session.uid, admin_id, ['name']
-            )['name']
-        )
-
-    def assertAdminPresentWithV8API(self):
-        self.assertEqual(
-            u"Administrator",
+    def assertAdminPresent(self):
+        self.assertIn(
+            u"Admin",
             self.session.env['res.users'].search(
                 [('login', '=', 'admin')]
             ).name
@@ -64,20 +46,7 @@ class SessionTestCase(TestCase):
         self.assertTrue(self.session.env.context.get('tz'))
 
     def test_registry(self):
-        # If version 8, 9 registry should be working
-        self.assertAdminPresentWithV8API()
-        if version_info[0] >= 10:
-            # Type error because we a registry exists and return
-            # an odoo.api.res.users class which does not know about
-            # old api signature methods, we are getting an error likes::
-            #
-            #    TypeError: unbound method search() must be called with
-            #    res.users instance as first argument (got Cursor instance
-            #    instead)
-            with self.assertRaises(TypeError):
-                self.assertAdminPresentWithV7API()
-        else:
-            self.assertAdminPresentWithV7API()
+        self.assertAdminPresent()
 
     def assertModuleState(self, module_name, expected_state):
         self.assertEqual(
